66baf58b33a06142c7be185eed35eaeb72790fed
Stefanie Kopp <stefanie.kopp@iteratec.com>
2025-05-16 15:48:46 +0200

    update config
    Signed-off-by: Stefanie Kopp <stefanie.kopp@iteratec.com>

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      filename: "service-worker.js",
       manifest: {
         name: "Trading App",
         short_name: "Trading",
