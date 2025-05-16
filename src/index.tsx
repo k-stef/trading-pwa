25e15a88878bafed88829c73fa35eef4af7d313d
Stefanie Kopp <stefanie.kopp@iteratec.com>
2025-05-16 15:22:05 +0200

    update config
    Signed-off-by: Stefanie Kopp <stefanie.kopp@iteratec.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -9,8 +9,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 );
 
 // Register service worker if supported
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(
+      `${import.meta.env.BASE_URL}service-worker.js`
+    );
   });
 }
