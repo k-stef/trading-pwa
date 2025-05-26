b6f12e055cdf104136ca89807df1d2dca3b656cb
Stefanie Kopp <stefanie.kopp@iteratec.com>
2025-05-26 11:18:52 +0200

    add rrr tests

--- src/components/tools/rrr-calc/RRRCalc.test.tsx
+++ src/components/tools/rrr-calc/RRRCalc.test.tsx
@@ -1,50 +1,53 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {RRRCalc} from './RRRCalc';
 
 test('renders RRRCalc component correctly', () => {
     render(<RRRCalc/>);
+    expect(screen.getByLabelText(/Entry Price/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Stop-Loss Price/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Take-Profit Price/i)).toBeInTheDocument();
     expect(screen.getByText(/Risk Reward Ratio \(Fees Included\) :/i)).toBeInTheDocument();
 });
 
-// test('calculates RRR correctly', () => {
-//     render(<RRRCalc/>);
-//
-//     const entryPriceInput = screen.getByPlaceholderText(/entry price/i);
-//     const stopLossPriceInput = screen.getByPlaceholderText(/stop loss price/i);
-//     const takeProfitPriceInput = screen.getByPlaceholderText(/take profit price/i);
-//
-//     fireEvent.change(entryPriceInput, {target: {value: '100'}});
-//     fireEvent.change(stopLossPriceInput, {target: {value: '90'}});
-//     fireEvent.change(takeProfitPriceInput, {target: {value: '110'}});
-//
-//     expect(screen.getByText(/1.00/i)).toBeInTheDocument();
-// });
-//
-// test('shows error when stop loss is not lower than entry', () => {
-//     render(<RRRCalc/>);
-//
-//     const entryPriceInput = screen.getByPlaceholderText(/entry price/i);
-//     const stopLossPriceInput = screen.getByPlaceholderText(/stop loss price/i);
-//     const takeProfitPriceInput = screen.getByPlaceholderText(/take profit price/i);
-//
-//     fireEvent.change(entryPriceInput, {target: {value: '100'}});
-//     fireEvent.change(stopLossPriceInput, {target: {value: '110'}});
-//     fireEvent.change(takeProfitPriceInput, {target: {value: '110'}});
-//
-//     expect(screen.getByText(/stop-loss price must be lower than entry price./i)).toBeInTheDocument();
-// });
-//
-// test('shows error when take profit is not higher than entry', () => {
-//     render(<RRRCalc/>);
-//
-//     const entryPriceInput = screen.getByPlaceholderText(/entry price/i);
-//     const stopLossPriceInput = screen.getByPlaceholderText(/stop loss price/i);
-//     const takeProfitPriceInput = screen.getByPlaceholderText(/take profit price/i);
-//
-//     fireEvent.change(entryPriceInput, {target: {value: '100'}});
-//     fireEvent.change(stopLossPriceInput, {target: {value: '90'}});
-//     fireEvent.change(takeProfitPriceInput, {target: {value: '90'}});
-//
-//     expect(screen.getByText(/take-profit price must be higher than entry price./i)).toBeInTheDocument();
-// });
+test('calculates RRR correctly', () => {
+    render(<RRRCalc/>);
+
+    const entryPriceInput = screen.getByLabelText(/entry price/i);
+    const stopLossPriceInput = screen.getByLabelText(/stop-loss price/i);
+    const takeProfitPriceInput = screen.getByLabelText(/take-profit price/i);
+
+    fireEvent.change(entryPriceInput, {target: {value: '10'}});
+    fireEvent.change(stopLossPriceInput, {target: {value: '8'}});
+    fireEvent.change(takeProfitPriceInput, {target: {value: '14'}});
+
+    expect(screen.getByText(/1.00/i)).toBeInTheDocument();
+});
+
+test('shows error when stop loss is not lower than entry', () => {
+    render(<RRRCalc/>);
+
+    const entryPriceInput = screen.getByLabelText(/entry price/i);
+    const stopLossPriceInput = screen.getByLabelText(/stop-loss price/i);
+    const takeProfitPriceInput = screen.getByLabelText(/take-profit price/i);
+
+    fireEvent.change(entryPriceInput, {target: {value: '10'}});
+    fireEvent.change(stopLossPriceInput, {target: {value: '10'}});
+    fireEvent.change(takeProfitPriceInput, {target: {value: '14'}});
+
+    expect(screen.getByText(/stop-loss price must be lower than entry price./i)).toBeInTheDocument();
+});
+
+test('shows error when take profit is not higher than entry', () => {
+    render(<RRRCalc/>);
+
+    const entryPriceInput = screen.getByLabelText(/entry price/i);
+    const stopLossPriceInput = screen.getByLabelText(/stop-loss price/i);
+    const takeProfitPriceInput = screen.getByLabelText(/take-profit price/i);
+
+    fireEvent.change(entryPriceInput, {target: {value: '10'}});
+    fireEvent.change(stopLossPriceInput, {target: {value: '8'}});
+    fireEvent.change(takeProfitPriceInput, {target: {value: '10'}});
+
+    expect(screen.getByText(/take-profit price must be higher than entry price./i)).toBeInTheDocument();
+});
